271f6c21711ac49d467f07a109de080c3212f17d
Eric Crescioni <ecam900@gmail.com>
2019-03-02 17:22:28 -0400

    Finish up About page and PropTypes

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -20,15 +20,28 @@ export class TodoItem extends Component {
         <p>
           <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> { ' ' }
           { title }
+          <button style={btnStyle} onClick={this.props.delTodo.bind(this, id)}>x</button>
         </p>
       </div>
     )
   }
 }
 
+// Prop Types
 TodoItem.propTypes = {
-  todo: PropTypes.object.isRequired
+  todo: PropTypes.object.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired
 }
 
+const btnStyle = {
+  background: '#ff0000',
+  color: '#fff',
+  border: 'none',
+  padding: '5px 10px',
+  borderRadius: '50%',
+  cursor: 'pointer',
+  float: 'right'
+}
 
 export default TodoItem;
